6b9e56203d87ceeb5a9b6fd97c3ba41ed010730e
toufiqnuur <taufik2017yk@gmail.com>
2025-11-06 12:35:16 +0700

    bump: astro version and remove vercel adapater

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 import icon from 'astro-icon'
 
-import vercel from '@astrojs/vercel'
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [icon()],
-	adapter: vercel(),
 	vite: {
 		plugins: [tailwindcss()]
 	}
