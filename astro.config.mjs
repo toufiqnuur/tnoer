fdedae89ec480fef037ce767713d510f69bc22b1
toufiqnuur <taufik2017yk@gmail.com>
2025-11-20 20:29:00 +0700

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import svelte from '@astrojs/svelte'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte()],
+	site: 'https://tnoer.pages.dev',
+	integrations: [svelte(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()]
 	}
 })
+
